97b815c2051cb97cb2bbb2634dc210183936596a
Chinonso Johnson <johnsonchinonso19@gmail.com>
2023-04-15 00:55:31 +0100

    adjusted the truncate in the team name component

--- src/components/TeamName.jsx
+++ src/components/TeamName.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React from 'react';
 
 const TeamName = ({ name }) => {
   return (
     <span
-      className={`text-base  md-truncate-8 sm:text-sm md:text-sm lg:text-lg font-semibold truncate  w-16 sm:w-20 md:w-28 lg:w-32 block`}
+      className={`text-base
+      truncate-18-lg
+      truncate-10-sm
+      md-truncate-8
+      sm:text-sm
+      md:text-sm
+      lg:text-lg
+      font-semibold
+      w-16
+      sm:w-20
+      md:w-28
+      lg:w-32
+      block`}
     >
       {name}
     </span>
