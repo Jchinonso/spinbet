b6876fd2aa0b8d1a1a568aedf6449aa2c00ee684
Chinonso Johnson <johnsonchinonso19@gmail.com>
2023-04-14 23:44:21 +0100

    initial commit

--- src/components/LiveStatus.jsx
+++ src/components/LiveStatus.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import classNames from "classnames";
 import { getStatusTextAndColor } from "@/utils/getStatusTextAndColor";
 
 const LiveStatus = ({ node }) => {
   const { statusText, textColor } = getStatusTextAndColor(node);
-  return <span className="text-sm block mb-3" style={{color: textColor}}>{statusText}</span>;
+  return <span className="text-sm block mb-3" style={{color: textColor}}>{statusText.toLocaleUpperCase()}</span>;
 };
 
 export default React.memo(LiveStatus);
