bea3bb4fdf96a8c7086e10c1cfe65753982c06fb
Chinonso Johnson <johnsonchinonso19@gmail.com>
2023-04-15 01:14:36 +0100

    add graphql playground

--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -1,8 +1,15 @@
-import { ApolloServer } from "@apollo/server";
-import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import { typeDefs } from "../../server";
-import { resolvers } from "../../server";
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { typeDefs } from '../../server';
+import { resolvers } from '../../server';
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true, // Enable introspection in production
+  playground: process.env.NODE_ENV === 'production' && {
+    endpoint: '/api/graphql', 
+  },
+});
 
 export default startServerAndCreateNextHandler(apolloServer);
