95d00594fc5172a69bd3a98778276643705ea53d
Chinonso Johnson <johnsonchinonso19@gmail.com>
2023-04-15 20:44:52 +0100

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4 text-white font-bold font-barlow flex justify-center items-center">
-      <h1 className="text-3xl">Spinbet Betting Platform</h1>
+    <nav className="bg-gray-800 px-4 py-2 md:py-4 text-white font-bold font-barlow flex justify-center items-center">
+      <h1 className="text-xl md:text-2xl lg:text-3xl">
+        Spinbet Betting Platform
+      </h1>
     </nav>
   );
 };
--- src/components/Navbar.test.jsx
+++ src/components/Navbar.test.jsx
@@ -8,14 +8,13 @@ describe('Navbar', () => {
     expect(navElement).toBeInTheDocument();
 
     expect(navElement).toHaveClass(
-      'bg-gray-800 p-4 text-white font-bold font-barlow flex justify-center items-center'
+      'bg-gray-800 px-4 py-2 md:py-4 text-white font-bold font-barlow flex justify-center items-center'
     );
-
-    // Check if the h1 element is rendered with the correct text
     const titleElement = screen.getByRole('heading', {
       level: 1,
       name: /Spinbet Betting Platform/i,
     });
     expect(titleElement).toBeInTheDocument();
+    expect(titleElement).toHaveClass('text-xl md:text-2xl lg:text-3xl');
   });
 });
